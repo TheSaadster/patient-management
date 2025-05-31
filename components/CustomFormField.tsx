9d3d851562204e45de76b1c46c88815938880351
Saad Aulakh <saadaulakh04@gmail.com>
2025-05-31 19:07:25 -0400

    Added email and phone number inputs

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -13,7 +13,10 @@ import {
 import { Input } from "@/components/ui/input"
 import { Control } from 'react-hook-form'
 import { FormFieldType } from './forms/PatientForm'
-
+import Image from 'next/image'
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+import { E164Number } from 'libphonenumber-js/core'
 interface CustomProps {
     control: Control<any>,
     fieldType: FormFieldType,
@@ -30,12 +33,43 @@ interface CustomProps {
 }
 
 const RenderField = ({ field, props }: { field: any, props: CustomProps }) => {
+    const { fieldType, iconSrc, iconAlt, placeholder} = props;
     switch (props.fieldType) {
         case FormFieldType.INPUT:
-            return(
+            return (
                 <div className='flex rounded-md border border-dark-500 bg-dark-400'>
-
+                    {iconSrc && (
+                        <Image
+                            src={iconSrc}
+                            height={24}
+                            width={24}
+                            alt={iconAlt || 'icon'}
+                            className='ml-2'
+                        />
+                    )}
+                    <FormControl>
+                        <Input 
+                            placeholder={placeholder}
+                            {...field}
+                            className='shad-input border-0'
+                        />
+                    </FormControl>
                 </div>
+            )
+        case FormFieldType.PHONE_INPUT:
+            return (
+                <FormControl>
+                    <PhoneInput
+                        defaultCountry='US'
+                        placeholder={placeholder}
+                        international
+                        withCountryCallingCode
+                        value = {field.value as E164Number | undefined}
+                        onChange={field.onChange}
+                        className='input-phone'
+
+                    />
+                </FormControl>
             )
     }
 }
